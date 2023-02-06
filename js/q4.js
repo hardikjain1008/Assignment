2e42eb25eaf09c594559756a281f55ec4e09e84c
Hardik Jain <hardik.jain@celigo.com>
2023-02-06 11:19:04 +0530

    js done

--- js/q4.js
+++ js/q4.js
@@ -6,6 +6,29 @@ Has a private function to sort the array.
 Takes as an input to construct an array of numbers
 Define another constructor function (SortObjectArray) which extends SortArray, and is used to sort array of JSON object
 */
+class SortArray {
+    constructor(originalArray) {
+      this.originalArray = originalArray;
+    }
+  
+    getSortedArray() {
+      return this._sortArray(this.originalArray);
+    }
+  
+    _sortArray(array) {
+      return array.sort((a, b) => a - b);
+    }
+  }
+  
+  class SortObjectArray extends SortArray {
+    _sortArray(array) {
+      return array.sort((a, b) => {
+        const propA = Object.values(a)[0];
+        const propB = Object.values(b)[0];
+        return propA - propB;
+      });
+    }
+  }
 
 class SortArray {
     constructor(originalArray) {
